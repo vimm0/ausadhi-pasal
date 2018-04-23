312500b8f3b7f9d968f86ebcf783d89663acc187
vimm0 <vimmrana0@gmail.com>
2018-04-23 22:26:12 +0545

    mf
    - change product's user to foreignkey
    - enhance template with materialize css
    - refactor view in inventory

--- apps/inventory/views.py
+++ apps/inventory/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render, get_object_or_404
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from apps.inventory.models import InventoryItem
+from apps.product.models import Product
 
 
 class Home(TemplateView):
     template_name = 'inventory/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        content_type = ContentType.objects.get_for_model(Product)
+        context['item_type'] = ContentType.objects.get_for_model(Product)
+        context['item_list'] = InventoryItem.objects.filter(content_type=content_type)
+        return context
--- apps/product/models.py
+++ apps/product/models.py
@@ -6,4 +6,7 @@ from ..users.models import User
 class Product(models.Model):
     name = models.CharField(max_length=225)
     price_per_unit = models.DecimalField(max_digits=25, decimal_places=2)
-    supplier = models.OneToOneField(User, on_delete=models.CASCADE)
+    supplier = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
